69e7d7538ff62c487e36d0e0f5f2fd3cbebd215f
seanwayland <seanwayland@gmail.com>
2021-06-23 22:12:02 -0400

    added core audio server for osx

--- pyotest.py
+++ pyotest.py
@@ -1,70 +1,64 @@
+
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+3 oscillators FM synthesis class.
+
+"""
 from pyo import *
-pm_list_devices()
 import math
 import sys
-import random
-random.seed(a=None, version=2)
+pm_list_devices()
 
+attacksetting = 0.001
 
-attacksetting = 0.015
+decaysetting= 0.3
 
-decaysetting = 0.3
+sustainsetting= 0.9
 
-sustainsetting = 0.9
+releasesetting= 0.07
 
-releasesetting = 0.05
+polyphony= 8
 
-polyphony = 8
+bendrange= 2
 
-bendrange = 2
-
-samplerate = 96000
+samplerate = 44100
 
 buffsize = 512
-s = Server(sr=samplerate, buffersize=buffsize)
+s = Server(audio="coreaudio", sr=samplerate, buffersize=buffsize)
 s.setMidiInputDevice(2)
-
 s.boot()
 
-n = Notein(poly=polyphony, scale=1)  # transpo
+n = Notein(poly=polyphony,scale=1) # transpo
 bend = Bendin(brange=bendrange, scale=1)
 env = MidiAdsr(n['velocity'], attack=attacksetting, decay=decaysetting, sustain=sustainsetting, release=releasesetting)
-t = HarmTable([1, 0.1])
+t = HarmTable([1,0.1])
 pit = n["pitch"]
 vel = n["velocity"]
 
 
+class FM3:
 
+    def __init__(self, fcar, ratio1, ratio2, index1, index2, out=0):
+        self.fmod = fcar * ratio1
+        self.fmodmod = self.fmod * ratio2
+        self.amod = self.fmod*index1
+        self.amodmod = self.fmodmod * index2
+        self.modmod = Sine(self.fmodmod, mul=self.amodmod)
+        self.mod = Sine(self.fmod+self.modmod, mul=self.amod)
+        self.car = Osc(t, fcar+self.mod, mul=env)
+        self.eq = EQ(self.car, freq=fcar, q=0.707, boost=-12)
+        self.filt = ButLP(self.eq, freq = 700+vel*3)
+        #self.fx2 = STRev(self.eq, inpos=0.25, revtime=2, cutoff=5000, mul=env, bal=0.01, roomSize=1)
+        #self.out = DCBlock(self.out).out(out)
+        #self.eq = EQ(self.car, freq=fcar, q=0.707, boost=-12)
+        
+        self.out = DCBlock(self.filt).out(out)
 
-
-
-#f = FM(carrier=[400,400], ratio=[.2498, .2503], index = [4.8, 3.8],  mul=0.2 ).out()
-
-freq = pit*bend
-# LFO applied to the `sharp` attribute
-# band limited saw wave
-#osc = LFO(freq=freq, type = 1 , sharp=0.7, mul=env)
-#f = FM(carrier=[pit,pit], ratio=[.2498, .2503], index = [4.8, 3.8],  mul=env)
-f = FM(carrier=pit*bend, ratio=.5 , index = 4 + vel*3,  mul=env)
-butlp = ButLP(f, 3000)
-mm = Mixer(outs=3, chnls=2, time=.025)
-fx2 = STRev(mm[0], inpos=0.25, revtime=0.1, cutoff=5000*vel - pit/10, mul=env, bal=0.06, roomSize=1).out(0)
-
-lfo = Sine(.1).range(0.78, 0.82)
-ph = Phasor(pit*bend)
-sqr = ph < lfo
-bisqr = Sig(sqr, mul=2, add=-1)
-filt = IRWinSinc(bisqr, freq=0, order=16)
-output = Sig(filt, mul=env)
-fx2 = STRev(mm[1], inpos=0.25, revtime=2, cutoff=5000, mul=env/2, bal=0.01, roomSize=1).out(1)
-
-
-mm.addInput(0, f)
-mm.addInput(1, output)
-mm.setAmp(0,0,.5)
-mm.setAmp(0,1,.5)
-mm.setAmp(1,1,.5)
-
+a = FM3(pit*bend, 0.33001, 2.9993, 8, 4, 0)
+b = FM3(pit*bend +0.12, 0.33003, 2.9992, 8, 4, 1)
+c = FM3(pit*2*bend - 0.1 , 0.33004, 2.9995, 8, 4, 0)
+d = FM3(pit*2*bend - 0.11, 0.33006, 2.9991, 8, 4, 1)
 
 
 s.gui(locals())
